af74610a8361a63089e8d6ab220055cfde01b254
Morgan Thomas <m@m0rg.dev>
2021-12-28 00:04:54 -0800

    taskwarrior

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd $(dirname $0)
 set -x
-cd emacs; ls -A | xargs realpath | xargs -I{} ln -svf {} ~/; cd -
-cd zsh; ls -A | xargs realpath | xargs -I{} ln -svf {} ~/; cd -
-cd tmux; ls -A | xargs realpath | xargs -I{} ln -svf {} ~/; cd -
+for dir in emacs zsh tmux task; do
+    cd $dir; ls -A | xargs realpath | xargs -I{} ln -svf {} ~/; cd -
+done
 test -e ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k || git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
