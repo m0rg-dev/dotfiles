6c36122f4c4be88e9e7ab20574ccc1ec2d9ea6eb
Morgan Thomas <m@m0rg.dev>
2021-12-27 22:38:41 -0800

    add tmux config

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
+#!/usr/bin/env bash
 cd $(dirname $0)
+set -x
 cd emacs; ls -A | xargs realpath | xargs -I{} ln -svf {} ~/; cd -
 cd zsh; ls -A | xargs realpath | xargs -I{} ln -svf {} ~/; cd -
-test -n ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+cd tmux; ls -A | xargs realpath | xargs -I{} ln -svf {} ~/; cd -
+test -e ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k || git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
