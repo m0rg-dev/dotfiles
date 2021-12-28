56ef12396f6e994d17b4f65d1175c5286746f613
Morgan Thomas <m@m0rg.dev>
2021-12-28 06:37:03 +0000

    add p10k to setup script

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
+cd $(dirname $0)
 cd emacs; ls -A | xargs realpath | xargs -I{} ln -svf {} ~/; cd -
 cd zsh; ls -A | xargs realpath | xargs -I{} ln -svf {} ~/; cd -
+test -n ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
