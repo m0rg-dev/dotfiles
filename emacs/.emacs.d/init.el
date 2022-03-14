f6a1572528a11a36f5775c1a8088da561b6fd514
Morgan Thomas <corp@m0rg.dev>
2022-03-13 22:07:18 -0700

    more init.el rework! cross-system solarized theme

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,3 +1,15 @@
+;;; init --- Morgan's base Emacs configuration
+;;;
+;;; Commentary:
+;;;
+;;;
+;;; Code:
+
+;; Pull in any local customizations.
+(let ((local-settings "~/.emacs.local.el"))
+  (when (file-exists-p local-settings)
+    (load-file local-settings)))
+
 (if (fboundp 'tool-bar-mode)
     (tool-bar-mode -1))
 
@@ -17,8 +29,6 @@
   (auto-package-update-maybe))
 
 (use-package terraform-mode)
-(use-package powerline
-  :config (powerline-default-theme))
 (use-package go-mode)
 (use-package nasm-mode)
 (use-package markdown-mode)
@@ -50,11 +60,13 @@
 (use-package lsp-treemacs :config (lsp-treemacs-sync-mode 1))
 
 (winner-mode)
-
-(require 'display-line-numbers)
+(column-number-mode)
 
 (require 'lsp-mode)
 (add-hook 'rust-mode-hook #'lsp)
+(add-hook 'go-mode-hook #'lsp)
+
+(require 'display-line-numbers)
 
 (defcustom display-line-numbers-exempt-modes
   '(vterm-mode eshell-mode shell-mode term-mode ansi-term-mode)
@@ -73,14 +85,99 @@ Exempt major modes are defined in `display-line-numbers-exempt-modes'."
 (global-display-line-numbers-mode)
 
 ;; bind mouse scroll
-(defun mouse-scroll-down () (interactive) (scroll-down 2))
-(defun mouse-scroll-up () (interactive) (scroll-up 2))
+(defun mouse-scroll-down () "Scroll down faster." (interactive) (scroll-down 2))
+(defun mouse-scroll-up () "Scroll up faster." (interactive) (scroll-up 2))
 
 (unless (display-graphic-p)
   (global-set-key (kbd "<mouse-4>") 'mouse-scroll-down)
   (global-set-key (kbd "<mouse-5>") 'mouse-scroll-up))
 
 (eval-after-load 'lsp '(define-key lsp-mode-map (kbd "d") 'lsp-ui-doc-show))
+
+;;; Face customization.
+
+(progn (setq frame-background-mode 'light)
+ (mapc 'frame-set-background-mode (frame-list)))
+
+(progn (setq frame-background-mode 'dark)
+ (mapc 'frame-set-background-mode (frame-list)))
+
+(defvar tty-specific-colors)
+(setq tty-specific-colors (if (display-graphic-p) '((:base03  . "#002b36")
+						    (:base02  . "#073642")
+						    (:base01  . "#586e75")
+						    (:base00  . "#657b83")
+						    (:base0   . "#839496")
+						    (:base1   . "#93a1a1")
+						    (:base2   . "#eee8d5")
+						    (:base3   . "#fdf6e3")
+						    (:yellow  . "#b58900")
+						    (:orange  . "#cb4b16")
+						    (:red     . "#dc322f")
+						    (:magenta . "#d33682")
+						    (:violet  . "#6c71c4")
+						    (:blue    . "#268bd2")
+						    (:cyan    . "#2aa198")
+						    (:green   . "#859900"))
+			    '((:base03  . "black")
+			      (:base02  . "brightblack")
+			      (:base01  . "brightgreen")
+			      (:base00  . "brightyellow")
+			      (:base0   . "brightblue")
+			      (:base1   . "brightcyan")
+			      (:base2   . "white")
+			      (:base3   . "brightwhite")
+			      (:yellow  . "yellow")
+			      (:orange  . "brightred")
+			      (:red     . "red")
+			      (:magenta . "magenta")
+			      (:violet  . "brightmagenta")
+			      (:blue    . "blue")
+			      (:cyan    . "cyan")
+			      (:green   . "green"))))
+
+(defvar named-colors)
+(setq named-colors (append tty-specific-colors '()))
+
+(defun color (sym) "Get SYM out of named-colors (shorthand)." (alist-get sym named-colors))
+
+(face-spec-set 'default `(
+			  (((background light)) (:foreground ,(color :base00) :background ,(color :base3) :family "Iosevka" :height 160))
+			  (((background dark)) (:foreground ,(color :base0) :background ,(color :base03) :family "Iosevka" :height 160))
+			  ))
+
+(defun face (name &rest args) "Set face NAME's default face spec to ARGS across all terminal types."
+       (face-spec-set name `((t ,args)) 'face-defface-spec))
+
+(defun dynamic-face (name light-only dark-only &rest both) "Set face NAME's default face spec to BOTH plus LIGHT-ONLY on light frames, and BOTH plus DARK-ONLY on dark frames."
+       (face-spec-set name `(
+			     (((background light)) ,(append light-only both))
+			      (((background dark)) ,(append dark-only both)))
+			   'face-defface-spec))
+
+(face 	      'error :foreground (color :red) :slant 'italic)
+(face 	      'font-lock-builtin-face :foreground (color :violet) :slant 'italic)
+(dynamic-face 'font-lock-comment-face (list :foreground (color :base1)) (list :foreground (color :base01)) :slant 'italic)
+(face 	      'font-lock-constant-face :foreground (color :cyan))
+(face 	      'font-lock-function-name-face :foreground (color :blue) :weight 'bold)
+(face 	      'font-lock-keyword-face :foreground (color :orange))
+(face 	      'font-lock-type-face :foreground (color :green))
+(face 	      'font-lock-string-face :foreground (color :cyan))
+(face 	      'font-lock-variable-name-face :foreground (color :yellow))
+(face 	      'header-line :underline t)
+(dynamic-face 'line-number (list :foreground (color :base1)) (list :foreground (color :base01)) :weight 'light)
+(face         'line-number-current-line :inherit 'line-number :weight 'bold)
+(face         'lsp-face-semhl-interface nil)
+(face         'minibuffer-prompt :foreground (color :blue) :weight 'bold)
+(face         'mode-line :foreground (color :base2) :background (color :violet))
+(dynamic-face 'mode-line-inactive (list :background (color :base2)) (list :background (color :base02)) :foreground (color :violet) :weight 'light)
+(face         'rust-string-interpolation :inherit 'font-lock-builtin-face)
+(dynamic-face 'shadow (list :foreground (color :base1)) (list :foreground (color :base01)) nil)
+(face         'success :foreground (color :green) :slant 'italic)
+(dynamic-face 'tty-menu-enabled-face (list :background (color :base2) :foreground (color :base00)) (list :background (color :base02) :foreground (color :base0)) :weight 'bold)
+(dynamic-face 'tty-menu-disabled-face (list :background (color :base2) :foreground (color :base01)) (list :background (color :base02) :foreground (color :base1)) :weight 'bold)
+(face         'tty-menu-selected-face :foreground (color :orange) :inherit 'tty-menu-enabled-face)
+(face         'warning :foreground "DarkOrange" :slant 'italic)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -106,39 +203,5 @@ Exempt major modes are defined in `display-line-numbers-exempt-modes'."
  '(vterm-buffer-name-string "vterm %s")
  '(xterm-mouse-mode t))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-variable-tag ((t (:foreground "blue" :weight bold))))
- '(error ((t (:foreground "Red1" :slant italic))))
- '(font-lock-builtin-face ((t (:foreground "brightmagenta" :slant italic))))
- '(font-lock-comment-face ((t (:foreground "red" :slant italic))))
- '(font-lock-constant-face ((t (:foreground "cyan"))))
- '(font-lock-function-name-face ((t (:foreground "blue"))))
- '(font-lock-keyword-face ((t (:foreground "brightred"))))
- '(font-lock-string-face ((t (:foreground "magenta"))))
- '(font-lock-type-face ((t (:foreground "green"))))
- '(font-lock-variable-name-face ((t (:foreground "yellow"))))
- '(header-line ((t (:underline t))))
- '(highlight ((t (:background "white"))))
- '(line-number ((t (:inherit default :foreground "green"))))
- '(line-number-current-line ((t (:inherit line-number :weight bold))))
- '(lsp-face-highlight-textual ((t (:underline t))))
- '(lsp-face-semhl-interface ((t nil)))
- '(lsp-headerline-breadcrumb-path-error-face ((t (:inherit lsp-headerline-breadcrumb-path-face :underline (:color "Red1" :style wave)))))
- '(minibuffer-prompt ((t (:foreground "blue"))))
- '(mode-line ((t (:background "brightmagenta" :foreground "white" :box (:line-width -1 :style released-button)))))
- '(mode-line-inactive ((t (:inherit mode-line :background "brightblack" :foreground "brightmagenta" :box (:line-width -1 :color "grey75") :weight light))))
- '(powerline-active1 ((t (:inherit mode-line :background "white" :foreground "black"))))
- '(powerline-active2 ((t (:inherit mode-line :background "brightgreen" :foreground "white"))))
- '(powerline-inactive1 ((t (:inherit powerline-active1))))
- '(powerline-inactive2 ((t (:inherit powerline-active2))))
- '(rust-string-interpolation ((t (:inherit font-lock-builtin-face))))
- '(shadow ((t (:foreground "grey30"))))
- '(success ((t (:foreground "green" :slant italic))))
- '(tty-menu-disabled-face ((t (:background "blue" :foreground "brightblack"))))
- '(tty-menu-enabled-face ((t (:background "blue" :foreground "white" :weight bold))))
- '(tty-menu-selected-face ((t (:inherit tty-menu-enabled-face :foreground "gray70"))))
- '(warning ((t (:foreground "DarkOrange" :slant italic)))))
+(provide 'init)
+;;; init.el ends here
