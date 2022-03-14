a904d6f23e45f13ca8c1e5bf235a38dacbe596ec
Morgan Thomas <corp@m0rg.dev>
2022-03-14 08:31:39 -0700

    show auto-package-update results

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -25,7 +25,6 @@
 (use-package auto-package-update
   :config
   (setq auto-package-update-delete-old-versions t)
-  (setq auto-package-update-hide-results t)
   (auto-package-update-maybe))
 
 (use-package terraform-mode)
